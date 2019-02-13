3754876d62db17a7d921efc7f164cd3e94fea198
SarfrazBughio <47584891+SarfrazBughio@users.noreply.github.com>
2019-02-13 10:56:20 +0500

    some changings

--- PrimeExample.java
+++ PrimeExample.java
@@ -3,6 +3,7 @@ public class PrimeExample{
 	public static void main(String args[]){
 	
 	    int i,m=0,flag=0;
+		
 		int n=5;
 		m=n/2;
 		
@@ -15,13 +16,15 @@ public class PrimeExample{
             for(i=2;i<=m;i++) {
                 
                 if(n%i==0){
-                  System.out.println(n+" is not prime number");
+                 
+			System.out.println(n+" is not prime number");
                   flag=1;
                   break;
                 }
 			}
 
-            if(flag==0) { System.out.println(n+" is prime number"); }
+            if(flag==0){ 
+		    System.out.println(n+" is prime number"); }
         }
     }
 }	
